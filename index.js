da9452a93e5145f064d919fccfe280a22efc9549
MohammedFattay <63704387+MohammedFattay@users.noreply.github.com>
2020-04-20 02:44:50 +0200

    adding test function for sayHi

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function sayHi(name){
     return 'hi ${name}'
 }
+
+function test(){
+    const testsayHi = sayHi("mohammed")
+
+    if (typeof testsayHi === "string"){
+        console.log("passed")
+    } else {
+        console.log("failed")
+    }
+
+}
